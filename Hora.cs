4411d49e14af58c542c9dbd1c78a9dc7e1290404
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2023-10-24 19:42:01 +0300

    updait

--- Hora.cs
+++ Hora.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace Horazad
 {
-    internal class Hora
+    public class Hora
     {
+        private string name;
+        private int godini;
+        private double zaplata;
+        private string adres;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentOutOfRangeException("value");
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get { return godini; }
+            set
+            {
+                if(value<18)
+                {
+                    throw new ArgumentException("value");
+                }
+                godini = value;
+            }
+        }
+        public double Zaplata
+        {
+            get { return zaplata; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("value");
+                zaplata = value;
+            }
+        }
+        public string Adres
+        {
+            get { return adres; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("value");
+            }
+        }
+        public Hora()
+        {
+            Name = "Ivan";
+            Age = 20;
+            Zaplata = 1000;
+            Adres = "Karlovo";
+        }
+        public Hora(string name,int godini,double zaplata,string adres)
+        {
+            Name = name;
+            Age = godini;
+            Zaplata = zaplata;
+            Adres = adres;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Horazad
+﻿using System.Reflection.Metadata;
+
+namespace Horazad
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                List<Hora> hora=new List<Hora>();
+                int n = int.Parse(Console.ReadLine());
+                Hora h;
+                for(int i=0;i<n;i++)
+                {
+                    var danni = Console.ReadLine().Split();
+                    h = new Hora(danni[0], int.Parse(danni[1]), double.Parse(danni[2]), danni[3]);
+                    hora.Add(h);
+                }
+
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
